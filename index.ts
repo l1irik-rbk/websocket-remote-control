af0d08b4fd9f7c4f4590def16e304987fa066259
l1irik-rbk <kir.baranow2010@gmail.com>
2022-06-23 19:55:19 +0300

    feat: add mouse controls

--- index.ts
+++ index.ts
@@ -1,9 +1,53 @@
 import Jimp from 'jimp';
 import { httpServer } from './src/http_server/index';
 import robot from 'robotjs';
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (data) => {
+    const newData = data.toString().split(' ');
+    const command = newData[0];
+    const coords = +newData[1];
+    console.log('data', coords);
+    dataParse(command, ws, coords);
+    // const { x, y } = robot.getMousePos();
+    // ws.send(`mouse_position ${x},${y}`);
+  });
+});
+
+wss.on('close', () => {});
+
+const dataParse = (command: string, ws: WebSocket, coords: number) => {
+  const { x, y } = robot.getMousePos();
+
+  switch (command) {
+    case 'mouse_position':
+      ws.send(`mouse_position ${x},${y}`);
+      break;
+    case 'mouse_left':
+      ws.send(`mouse_left`);
+      robot.moveMouse(-coords + x, y);
+      break;
+    case 'mouse_right':
+      ws.send(`mouse_right`);
+      robot.moveMouse(coords + x, y);
+      break;
+    case 'mouse_down':
+      ws.send(`mouse_down`);
+      robot.moveMouse(x, coords + y);
+      break;
+    case 'mouse_up':
+      ws.send(`mouse_up`);
+      robot.moveMouse(x, -coords + y);
+      break;
+    default:
+      break;
+  }
+};
